3ab2e7a252dc68f42651cb89a39750b076a2c0a0
55973 <55973@etu.he2b.be>
2022-12-20 09:40:25 +0100

    enteri a 100

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,6 @@ console.log(Math.ceil(Math.random()*4));
 console.log(Math.floor(Math.random()*4));
 console.log(Math.random())
 console.log(Math.round(Math.random()));
-console.log(Math.round(Math.random()*1));
+console.log((Math.random()*1));
+console.log((Math.random()*100));
 console.log(Math.round(Math.random()*100));
