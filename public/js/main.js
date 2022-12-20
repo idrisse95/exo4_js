98c56124dc3a63b7a30f36e8a3f9588095268173
55973 <55973@etu.he2b.be>
2022-12-20 09:54:10 +0100

    fin

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,8 @@ console.log(Math.round(Math.random()));
 console.log((Math.random()*1));
 console.log((Math.random()*100));
 console.log(Math.round(Math.random()*100));
+console.log(Math.pow(8,2));
+console.log(Math.sqrt(9));
+console.log(Math.abs(-1));
+console.log(Math.max(-2,1000,8,57));
+console.log(Math.min(-2,1000,8,57));
